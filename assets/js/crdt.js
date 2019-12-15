430a086027f5a697d4cf9f5d4b5dce626babbf26
Shanthanu <shanthanu.s.rai9@gmail.com>
2019-12-15 22:25:07 +0530

    Add localInsert and localDelete to CRDT class

--- assets/js/crdt.js
+++ assets/js/crdt.js
@@ -144,6 +144,135 @@ class CRDT {
     }
 
     /**
+     * Find identifierList between `prevIdentifierList` and `nextIdentifierList`
+     * @param  {List{Identifier}} prevIdentifierList
+     * @param  {List{Identifier}} nextIdentifierList
+     * @result {List{Identifier}}
+     */
+    findNextGreaterIdentifierList(prevIdentifierList, nextIdentifierList, siteID) {
+        var maxLen = Math.max(prevIdentifierList.length, nextIdentifierList.length);
+        var newIdentifierList = []
+        
+        //Keep track of siteID before current Identifier
+        var lastPrevSiteID = -1;
+        var lastNextSiteID = Infinity;
+
+        //Keeps track if the identifierList is found
+        var identifierListFound = false;
+        //Keeps track if next greater identifier of prev identifier is being found out
+        var nextGreaterIdentifierFound = false;
+
+        //Iterate over prev and next Identifier and get IdentifierList of `insertCharacter`
+        for(let i = 0; i < maxLen; i++) {
+            //TODO: Check this
+            var prevIdentifier = ((i < prevIdentifierList.length) ? prevIdentifierList[i] : new Identifier(0, lastPrevSiteID));
+            var nextIdentifier = ((i < nextIdentifierList.length) ? nextIdentifierList[i] : new Identifier(0, lastNextSiteID));
+            
+            if(!nextGreaterIdentifierFound) {
+                if(prevIdentifier.position < nextIdentifier.position) {
+                    //Being greedy on size of identifier list
+                    if(siteID > prevIdentifier.siteID && prevIdentifier.siteID != -1) { //TODO: check if ch is same? idempotency?
+                        //If by siteID alone identifier order can be obtained, then push and done!
+                        //Edge case of first character inserted in line handled by 2nd condition
+                        newIdentifierList.push(new Identifier(prevIdentifier.position, siteID));
+                        identifierListFound = true;
+                    }
+                    else if(prevIdentifier.positon + 1 < nextIdentifier.position) {
+                        //If there is atleast a gap of 2 between prevIdentifier and nextIdentifier, take
+                        //prevIdentifier.position+1 and done!
+                        newIdentifierList.push(new Identifier(prevIdentifier.position+1, siteID));
+                        identifierListFound = true;
+                    }
+                    else { //prevIdentifier.position + 1 == nextIdentifier.position
+                        //IdentifierList lesser than nextIndentifierList is found;
+                        //Next find IdentifierList greater than prevIdentifier
+                        newIdentifierList.push(new Identifier(prevIdentifier.position, prevIdentifier.siteID));
+                        nextGreaterIdentifierFound = true;
+                    }
+                }
+                else { //prevIdentifier.position == nextIdentifer.position
+                    //NOTE: prevIdentifier.siteID < nextIdentifier.siteID
+                    if(prevIdentifier.siteID < siteID && siteID < nextIdentifier.siteID) {
+                        //By siteID alone order can be obtained, then push and done!
+                        newIdentifierList.push(new Identifier(prevIdentifier.position, siteID));
+                        identifierListFound = true;
+                    }
+                    else {
+                        //Positions are same and siteIDs don't help in ordering. Have to go ahead
+                        newIdentifierList.push(new Identifier(prevIdentifier.position, prevIdentifier.siteID));
+                    }
+                }
+            }
+            else {
+                //IdentifierList is already less then nextIdentifierList;
+                //This section finds IdentifierList greater than prevIdentifierList
+                if(siteID > prevIdentifier.siteID) {
+                    //By siteId alone order can be obtained, then push and done!
+                    newIdentifierList.push(new Identifier(prevIdentifier.position, siteID));
+                    identifierListFound = true;
+                }
+                else if(prevIdentifier.position < 9) {
+                    //If prevIdentifier.position is not 9, then can always add 1 to position, push and done!
+                    newIdentifierList.push(new Identifier(prevIdentifier.position+1, siteID));
+                    identifierListFound = true;
+                }
+                else { //prevIdentifier.position == 9
+                    //If prevIdentifier.position is 9, then have to go ahead
+                    newIdentifierList.push(new Identifier(prevIdentifier.position, prevIdentifier.siteID));
+                }
+            }
+
+            lastPrevSiteID = prevIdentifier.siteID;
+            lastNextSiteID = nextIdentifier.siteID;
+
+            if(identifierListFound) break;
+        }
+
+        if(!identifierListFound) {
+            newIdentifierList.push(new Identifier(1, siteID));
+            identifierListFound = true;
+        }
+
+        return newIdentifierList;
+    }
+
+    /**
+     * Insert `ch` at line `lineNumber` and position `pos` by `siteID`
+     * Enters at `pos`, characters after `pos` are shifted ahead
+     * @param  {Char}   ch
+     * @param  {Number} lineNumber
+     * @param  {Number} pos
+     * @param  {Number} siteID
+     * @result {Character}
+     */
+    localInsert(ch, lineNumber, pos, siteID) {
+
+        pos = pos + 1;
+
+        var prevIdentifierList = this.data[lineNumber][pos-1].identifiers;
+        var nextIdentifierList = this.data[lineNumber][pos].identifiers;
+        
+        var insertCharacter = new Character(ch, this.findNextGreaterIdentifierList(prevIdentifierList, nextIdentifierList, siteID));
+
+        //insert new Character to CRDT and return it
+        this.data[lineNumber].splice(pos, 0, insertCharacter);
+        return insertCharacter;
+    }
+
+     /**
+     * Delete `character` at line `lineNumber` and position `pos`
+     * @param  {Number} lineNumber
+     * @param  {Number} pos
+     * @result {Character}
+     */
+    localDelete(lineNumber, pos) {
+        pos = pos + 1;
+        var tempCharacter = this.data[lineNumber][pos];
+        this.data[lineNumber].splice(pos, 1);
+        return tempCharacter;
+    }
+
+    /**
      * Get string representation of `CRDT`.
      * Useful for debugging.
      */
