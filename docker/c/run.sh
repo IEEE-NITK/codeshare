793f6a0318e266072c4183c60aa9ceae4d300d74
Rohit M P <rohitmp99@gmail.com>
2020-03-08 18:19:53 +0530

    Set time limit only for execution

--- docker/c/run.sh
+++ docker/c/run.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 mv files/code files/code.c
-gcc files/code.c -o executable
+gcc files/code.c -o executable &> output
 if [[ $? -ne 0 ]]; then
+    mv output files/output
     exit 145 # Compilation error. Terminate.
 fi
 # ./executable < files/input
-./executable
+./executable &>> output &
+
+C_PID=$!
+sleep 2s
+if ps -p $C_PID > /dev/null
+then
+    kill $C_PID
+    echo "CODESHARE: Program took too long to run.. Exiting now." >> output
+fi
+
+mv output files/output
--- docker/python/run.sh
+++ docker/python/run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 mv files/code files/code.py
-python3 files/code.py
+python3 files/code.py &> output &
+
+PYTHON_PID=$!
+sleep 5s
+if ps -p $PYTHON_PID > /dev/null
+then
+    kill $PYTHON_PID
+    echo "CODESHARE: Program took too long to run.. Exiting now." >> output
+fi
+
+mv output files/output
