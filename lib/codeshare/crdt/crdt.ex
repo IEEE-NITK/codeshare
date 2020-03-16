98ff260679c2b3e09feaf37ed8a6dfebec19e70b
Shanthanu <shanthanu.s.rai9@gmail.com>
2020-03-16 20:33:43 +0530

    server_crdt: Implement all remote functions
    Testing of consistency of client and server CRDTs not done yet.

--- lib/codeshare/crdt/crdt.ex
+++ lib/codeshare/crdt/crdt.ex
@@ -51,20 +51,20 @@ defmodule Codeshare.CRDT do
   end
 
   defp remote_delete(character) do
-    "delete"
+    Agent.update(__MODULE__, fn crdt -> delete_character(crdt, character) end)
   end
 
   defp remote_insert_newline(character) do
-    "inputnewline"
+    Agent.update(__MODULE__, fn crdt -> insert_newline(crdt, character) end)
   end
 
   defp remote_delete_newline(character) do
-    "deletenewline"
+    Agent.update(__MODULE__, fn crdt -> delete_newline(crdt, character) end)
   end
 
   defp insert_character(crdt, character) do
     
-    [ first_line | crdt] = crdt
+    [first_line | crdt] = crdt
     last_ch = List.last(first_line) # NOTE: Takes linear time :(
 
     if Character.is_greater(last_ch, character) do
@@ -82,6 +82,75 @@ defmodule Codeshare.CRDT do
       [character | [ first_ch | line] ]
     else
       [first_ch | insert_character_on_line(line, character)]
+    end
+  end
+
+  defp delete_character(crdt, character) do
+    
+    [first_line | crdt] = crdt
+    last_ch = List.last(first_line) # NOTE: Takes linear time :(
+
+    if Character.is_greater(last_ch, character) do
+      [delete_character_on_line(first_line, character) | crdt]
+    else
+      [first_line | delete_character(crdt, character)]
+    end
+  end
+
+  defp delete_character_on_line(line, character) do
+
+    [first_ch | line] = line
+
+    if Character.is_equal(first_ch, character) do
+      line
+    else
+      [first_ch | delete_character_on_line(line, character)]
+    end
+  end
+
+  defp insert_newline(crdt, character) do
+    
+    [first_line | crdt] = crdt
+    last_ch = List.last(first_line) # NOTE: Takes linear time :(
+
+    if Character.is_greater(last_ch, character) do
+      insert_newline_on_line(first_line, character) ++ crdt
+    else
+      [first_line | insert_character(crdt, character)]
+    end
+  end
+
+  defp insert_newline_on_line(line, character) do
+    
+    [first_ch | line] = line
+
+    if Character.is_greater(first_ch, character) do
+      delimeter_ch = %Character{
+        ch: "",
+        identifiers: character.identifiers
+      }
+      line1 = [delimeter_ch]
+      line2 = [delimeter_ch | [first_ch | line] ]
+      [line1, line2]
+    else
+      lines = insert_newline_on_line(line, character)
+      [line1 | [line2]] = lines
+      line1 = [first_ch | line1]
+      [line1 | [line2]]
+    end
+  end
+
+  defp delete_newline(crdt, character) do
+    
+    [first_line | crdt] = crdt
+    last_ch = List.last(first_line) # NOTE: Takes linear time :(
+
+    if Character.is_equal(last_ch, character) do
+      line1 = first_line |> Enum.reverse |> tl |> Enum.reverse
+      [[_ | line2] | crdt] = crdt
+      [line1++line2 | crdt]
+    else
+      [first_line | insert_character(crdt, character)]
     end
   end
 
